543bb4b99b2bacdc9bb0213b9c65e116c419d4f1
Aaron Long <aaronlong0712@gmail.com>
2021-12-31 22:51:54 -0500

    Done for tonight, ill do more tomorrow

--- pages/Area27Roadmap.js
+++ pages/Area27Roadmap.js
@@ -1,7 +1,24 @@
+import styles from "../styles/Area27Roadmap.module.css"
+
+export async function getStaticProps() {
+  const res = await fetch("https://v2roadmap-default-rtdb.firebaseio.com/Roadmap.json")
+  const data = await res.json()
+
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+  return {
+    props: { data }, 
+  }
+}  
+
+
 export default function Area27Roadmap({ data }) {
     return(
       <>
-      
+        {/* <div className={styles.card}>asd</div> */}
       </>
     )
 }
