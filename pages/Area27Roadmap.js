56ab6e89ba3d33cd9c4bc493dac5bc9d8140bd6e
Aaron Long <aaronlong0712@gmail.com>
2022-01-13 22:40:16 -0500

    TOOK YA LONG ENOUGH

--- pages/Area27Roadmap.js
+++ pages/Area27Roadmap.js
@@ -12,13 +12,75 @@ export async function getStaticProps() {
   return {
     props: { data }, 
   }
-}  
+} 
 
+const Header = (props) => {
+  const headerText = props.index
+  return(
+    <h4 className={styles.updateText}>{headerText}</h4>
+  )
+}
+
+const Status = (props) => {
+  const status = props.status
+  
+  if (status == 0) {
+    return(
+      <span className={styles.status0}>Not Started</span>
+    )
+  } else if (status == 1){
+    return(
+      <span className={styles.status1}>In Progress</span>
+    )
+  } else if (status == 2){
+    return(
+      <span className={styles.status2}>Live</span>
+    )
+  } else if (status == 3){
+    return(
+      <span className={styles.status3}>Testing Phase</span>
+    )
+  } else if (status == 4){
+    return(
+      <span className={styles.status4}>Concept Phase</span>
+    )
+  } else {
+    return(
+      <></>
+    )
+  }
+}
+
+const Information = (props) => {
+  const data = props.data
+  const indexVar = props.index
+  return(
+    data[indexVar].map((obj) => (
+      <>
+      <div className={styles.spacer}></div>
+      <div className={`${styles.card} w3-card`}>
+        <div className={styles.container}>
+          <p className={styles.dataText}>{obj.name}</p>
+          <p className={styles.description}>{obj.description}</p>
+          <Status status={obj.status}/>
+        </div>
+      </div>
+      </>
+    ))
+  )
+}
 
 export default function Area27Roadmap({ data }) {
     return(
       <>
-        {/* <div className={styles.card}>asd</div> */}
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"></link>
+        {Object.keys(data).map((index)=>(
+          <>
+            <Header index={index} />
+            <Information index={index} data={data} />
+          </>
+        ))}
+        <div className={styles.spacer}></div>
       </>
     )
 }
